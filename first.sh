31b0b64808f5e26c28da3551fe746948de22d96d
SeelamKeerthiprasanna <148511023+SeelamKeerthiprasanna@users.noreply.github.com>
2026-04-02 15:20:18 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,2 +1,3 @@
 This is my first Activity in Github
 The name of the first Activity was SCM using GitHub
+The ALV portal i am using instructed to doing this activity in GitHub as name of the Activity says
